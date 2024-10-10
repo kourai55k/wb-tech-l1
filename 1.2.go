a27538df3661ad1169c49c4c2d2b3a97ae7cdfa7
kourai.55k <kourai.55k@gmail.com>
2024-10-10 16:10:44 +0300

    edit task 2

--- 1.2.go
+++ 1.2.go
@@ -1,27 +1,26 @@
 package main
 
-//
-//import (
-//	"fmt"
-//	"os"
-//	"sync"
-//)
-//
-//// Sq выводит квадрат числа в stdout
-//func Sq(n int, wg *sync.WaitGroup) {
-//	defer wg.Done() // Сообщаем, что горутина завершила свою работу
-//	fmt.Fprintln(os.Stdout, n*n)
-//}
-//
-//func main() {
-//	arr := []int{2, 4, 6, 8, 10}
-//	var wg sync.WaitGroup
-//
-//	// Проходим в цикле for по массиву и для каждого числа вычисляем квадрат в отдельной горутине
-//	for _, el := range arr {
-//		wg.Add(1) // Увеличиваем счетчик активных горутин
-//		go Sq(el, &wg)
-//	}
-//
-//	wg.Wait() // Ожидаем завершения всех горутин
-//}
+import (
+	"fmt"
+	"os"
+	"sync"
+)
+
+// Square выводит квадрат числа в stdout
+func Square(n int, wg *sync.WaitGroup) {
+	defer wg.Done() // Сообщаем, что горутина завершила свою работу
+	fmt.Fprintln(os.Stdout, n*n)
+}
+
+func main() {
+	arr := []int{2, 4, 6, 8, 10}
+	var wg sync.WaitGroup
+
+	// Проходим в цикле for по массиву и для каждого числа вычисляем квадрат в отдельной горутине
+	for _, el := range arr {
+		wg.Add(1) // Увеличиваем счетчик активных горутин
+		go Square(el, &wg)
+	}
+
+	wg.Wait() // Ожидаем завершения всех горутин
+}
